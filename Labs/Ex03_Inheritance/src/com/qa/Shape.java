18dfd01364977c6099a7f9b9f369d70a4f111187
Jinyi <jinyi.jin@rwth-aachen.de>
2023-07-25 14:47:59 +0100

    git the java projects

---
+++ Labs/Ex03_Inheritance/src/com/qa/Shape.java
@@ -0,0 +1,12 @@
+package com.qa;
+
+public class Shape {
+    private String name;
+    private String colour;
+    private double x, y;
+
+    public Shape(String name, String colour, double x, double y){
+        this.name = name;
+        this.colour = colour;
+    }
+}
---
+++ Labs/GreetFriend/src/Greet.java
@@ -0,0 +1,6 @@
+public class Greet {
+    public static void main(String[] args) {
+        String friendName = "John";
+        System.out.println("Greetings from the great class");
+    }
+}
